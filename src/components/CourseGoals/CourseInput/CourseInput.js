5c4f82b6ef4bce1353b208c07a570e0c3fec03c7
Szeplaki <szeplaki.eva1@gmail.com>
2023-04-19 13:39:19 +0200

    set the color back, if input is not empty

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -9,6 +9,9 @@ const CourseInput = (props) => {
 
 
   const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
@@ -24,8 +27,8 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label style={{color: !isValid ? "red" : "black"}}>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+        <input style={{borderColor: !isValid ? 'lightcoral' : '#ccc', background: !isValid ? 'pink' : 'transparent'}} type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
